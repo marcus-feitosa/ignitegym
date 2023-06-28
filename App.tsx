8eb412d2049e6610067e13192a790a752a66318f
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-28 19:47:34 -0300

    Aplicando tema

--- App.tsx
+++ App.tsx
@@ -3,13 +3,17 @@ import { Text, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'react-native';
+
+import { THEME } from './src/theme';
+
 import { Loading } from '@components/Loading';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
 
-    <NativeBaseProvider >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
       barStyle='light-content'
       backgroundColor='transparent'
