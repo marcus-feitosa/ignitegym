bfad82b3d189a23a2b7d483e03d6f86fb6632201
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-28 19:44:58 -0300

    Componente Loading

--- App.tsx
+++ App.tsx
@@ -3,18 +3,19 @@ import { Text, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
-    
+
     <NativeBaseProvider >
       <StatusBar 
       barStyle='light-content'
       backgroundColor='transparent'
       translucent
       />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
