7abdaab7d0a2bd61beeba32f916c0e65bc2d4b68
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-28 19:41:55 -0300

    Aplicando Native Base

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 
 import { Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
-    <View >
+    
+    <NativeBaseProvider >
       <StatusBar 
       barStyle='light-content'
       backgroundColor='transparent'
       translucent
       />
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </NativeBaseProvider>
   );
 }
 
