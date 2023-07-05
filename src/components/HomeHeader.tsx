42ed66453a4bb78662e27fbe95f0f0913c309770
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-07-05 14:23:14 -0300

    HomeHeader finalizado

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,35 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { UserPhoto } from './UserPhoto';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export function HomeHeader() {
   return (
-    <HStack>
-      <VStack>
-        <Text color="gray.100">
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+        <UserPhoto 
+        source={{ uri: 'https://github.com/marcus-feitosa.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
-          Rodrigo
+        <Heading color="gray.100" fontSize="md">
+          Marcus
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   );
 }
