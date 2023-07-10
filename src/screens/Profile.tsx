bb3b790ce03bf5a780897a3e11120c62830f4d63
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-07-10 18:19:38 -0300

    Efeito skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,37 @@
-import { Center, Text } from 'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+
+import { useState } from 'react';
+import { Center, ScrollView, VStack, Skeleton } from 'native-base';
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Perfil' />
+      <ScrollView>
+        <Center mt={6} px={10}>
+        {
+            photoIsLoading ?
+              <Skeleton 
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+            :
+              <UserPhoto 
+                source={{ uri: 'https://github.com/marcus-feitosa.png' }}
+                alt="Foto do usuário"
+                size={PHOTO_SIZE}
+              />
+          }
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
