9c411a10972e18c89f64e0a7a5796e98d45d2def
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-07-10 19:48:13 -0300

    Carregando foto da galerias

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,17 +1,63 @@
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-
-import { useState } from 'react';
-import { Center, ScrollView, VStack, Text, Skeleton, Heading, useTheme } from 'native-base';
-import { TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+
+import { useState } from 'react';
+import { Center, ScrollView, VStack, Text, Skeleton, Heading, useToast, useTheme } from 'native-base';
+import {TouchableOpacity } from 'react-native';
+
+import { FileInfo } from "expo-file-system";
+
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/marcus-feitosa.png');
+  
+  const toast = useToast();
+  async function handleUserPhotoSelected(){
+    
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if(photoSelected.canceled) {
+        return;
+      }
+      
+    if(photoSelected.assets[0].uri) {
+      const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileInfo;
+      if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 2){
+
+        return toast.show({
+          title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      setUserPhoto(photoSelected.assets[0].uri);
+    }
+
+  } catch (error) {
+    console.log(error)
+  } finally {
+    setPhotoIsLoading(false)
+  }
+}
+  
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
@@ -28,13 +74,13 @@ export function Profile() {
               />
             :
               <UserPhoto 
-                source={{ uri: 'https://github.com/marcus-feitosa.png' }}
+                source={{ uri: userPhoto }}
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar Foto
             </Text>
