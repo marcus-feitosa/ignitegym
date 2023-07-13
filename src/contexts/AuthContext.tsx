d8d88009eb01d7c336008db2919062607a617412
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-07-13 15:07:42 -0300

    Buscando usuário do backend

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,10 +1,10 @@
 import { ReactNode, createContext, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
-
+import { api } from '@services/api';
 
 export type AuthContextDataProps = {
   user: UserDTO;
-  singIn: (email: string, password: string) => void;
+  singIn: (email: string, password: string) => Promise<void>;
 }
 
 type AuthContextProviderProps = {
@@ -14,20 +14,18 @@ type AuthContextProviderProps = {
   export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
   
   export function AuthContextProvider({ children }: AuthContextProviderProps)  {
-    const [user, setUser] = useState({
-      id: '1',
-      name: 'Rodrigo ',
-      email: 'rodrigo@email.com',
-      avatar: 'rodrigo.png'
-    })
+    const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-    function singIn(email: string, password: string) {
-      setUser({
-        id: '',
-        name: '',
-        email,
-        avatar: '',
-      })
+    async function singIn(email: string, password: string) {
+      try {
+        const { data } = await api.post('/sessions', { email, password });
+
+        if(data.user) {
+          setUser(data.user);
+        }
+      } catch (error) {
+        throw error
+      }
     }
     return (
       <AuthContext.Provider value={{ user, singIn }}>
