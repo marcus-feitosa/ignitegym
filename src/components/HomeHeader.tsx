83a519c692f99fb1f50aab1f3d6f7d401ff22156
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-07-13 17:02:10 -0300

    Corrigindo HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,6 @@
 import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Text, VStack, Icon } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
-import { useAuth } from '@hooks/useAuth';
-import defaulUserPhotoImg from '@assets/userPhotoDefault.png'; 
 
 import { useAuth } from '@hooks/useAuth';
 
@@ -11,21 +9,12 @@ import defaulUserPhotoImg from '@assets/userPhotoDefault.png';
 import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
-<<<<<<< HEAD
 
   const { user, signOut } = useAuth();
 
-=======
-  const { user } = useAuth();
->>>>>>> 5624ebca409e332d4ced95fd332fc4ce4d8cc964
   return (
-    
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-<<<<<<< HEAD
       <UserPhoto 
-=======
-        <UserPhoto 
->>>>>>> 5624ebca409e332d4ced95fd332fc4ce4d8cc964
         source={user.avatar  ? { uri: user.avatar } : defaulUserPhotoImg}
         size={16}
         alt="Imagem do usuário"
@@ -36,6 +25,7 @@ export function HomeHeader() {
         <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
+
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
           {user.name}
         </Heading>
